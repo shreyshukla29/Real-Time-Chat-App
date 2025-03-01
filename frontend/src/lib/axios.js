e49c84a7a12ee6a32899bb94b95919b235d8cdf7
Shrey <shreyshukla101217@gmail.com>
2025-03-01 19:46:12 +0530

    fix error

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 const url = import.meta.env.VITE_BACKEND_URL
 export const axiosInstance = axios.create({
-  baseURL:  url,
+  baseURL:  url ,
   withCredentials: true,
 });
