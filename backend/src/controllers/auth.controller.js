cef9f8b5745bf07d992b65ee44cc1a075c139f0f
Shrey <shreyshukla101217@gmail.com>
2025-03-01 21:41:27 +0530

    resolve samesite error in logout controller

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -77,7 +77,12 @@ export const login = async (req, res) => {
 
 export const logout = (req, res) => {
   try {
-    res.cookie("jwt", "", { maxAge: 0 });
+    res.cookie("jwt", " ", {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // MS
+      httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+      sameSite: 'none', // CSRF attacks cross-site request forgery attacks
+      secure:true,
+    });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     console.log("Error in logout controller", error.message);
