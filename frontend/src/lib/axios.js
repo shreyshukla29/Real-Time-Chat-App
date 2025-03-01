17bb2c54a46a7ecc1e37b020006c6be8758f3256
Shrey <shreyshukla101217@gmail.com>
2025-03-01 19:15:35 +0530

    add env files

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
+const url = import.meta.env.VITE_BACKEND_URL
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? url : "/api",
   withCredentials: true,
 });
